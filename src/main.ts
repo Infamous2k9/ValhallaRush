919c80af6a904d811e0f8cb9845c267f1dc8719a
Andre Funk <andre.funk@icloud.com>
2026-02-20 11:58:03 +0100

    test: added for DevTools in chrome

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { WorldModel } from './models/world.model';
 import './styles/styles.scss';
+// damit game in den DevTools angezeigt wird!!!
+declare global {
+  interface Window {
+    game: WorldModel;
+  }
+}
+
 let appRef = document.querySelector<HTMLCanvasElement>('[data-app]');
 let game: WorldModel;
 
 function init() {
   if (!appRef) return;
   game = new WorldModel(appRef);
+
+  //devtools
+  window.game = game;
 }
 init();
